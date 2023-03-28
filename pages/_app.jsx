73901f3ff2eea3cf31a01859d76c6d963ad8893c
ProgrammerFront077 <alirezakh0090@gmail.com>
2023-03-28 16:32:19 +0430

    (Add):<-Head Component On _app.js->

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,47 @@
 import PageProvider from '@/MuiTheme/helpers/PageProvider';
+import { AppBar, Button, Stack, Toolbar } from '@mui/material';
+import Link from 'next/link';
 import '../styles/globals.css';
 export default function App({ Component, pageProps, emotionCache }) {
   return (
     <PageProvider emotionCache={emotionCache}>
-      <Component {...pageProps} />
+      <AppBar>
+        <Toolbar>
+          <Link style={{ textDecoration: 'none' }} href="/">
+            <Button
+              style={{ fontSize: '1.1rem' }}
+              variant="text"
+              color="info"
+              size="large"
+            >
+              صفحه اصلی
+            </Button>
+          </Link>
+          <Link style={{ textDecoration: 'none' }} href="/">
+            <Button
+              style={{ fontSize: '1.1rem' }}
+              variant="text"
+              color="info"
+              size="large"
+            >
+              درباره ما
+            </Button>
+          </Link>
+          <Link style={{ textDecoration: 'none' }} href="/">
+            <Button
+              style={{ fontSize: '1.1rem' }}
+              variant="text"
+              color="info"
+              size="large"
+            >
+              محصولات
+            </Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <Stack mt={10}>
+        <Component {...pageProps} />
+      </Stack>
     </PageProvider>
   );
 }
